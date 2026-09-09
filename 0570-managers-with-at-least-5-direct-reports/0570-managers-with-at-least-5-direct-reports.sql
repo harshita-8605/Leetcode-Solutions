b4a24894c1f279c514f1dac464ec77b8ca50881a
harshita-8605 <harshitanarula08@gmail.com>
2026-09-09 15:58:31 +0530

    Time: 459 ms (16.64%), Space: 0B (100%) - LeetHub

---
+++ 0570-managers-with-at-least-5-direct-reports/0570-managers-with-at-least-5-direct-reports.sql
@@ -0,0 +1,10 @@
+# Write your MySQL query statement below
+SELECT name
+FROM Employee AS E INNER JOIN (
+    SELECT managerId, COUNT(*)
+    FROM 
+    Employee AS E
+    GROUP BY managerId
+    HAVING COUNT(managerId) >= 5
+) AS M ON E.id = M.managerId;
+
