4501901309f77de337de529e570ae850d76ac2bf
harshita-8605 <harshitanarula08@gmail.com>
2026-09-10 19:01:10 +0530

    Time: 701 ms (69.28%), Space: 0B (100%) - LeetHub

--- 1934-confirmation-rate/1934-confirmation-rate.sql
+++ 1934-confirmation-rate/1934-confirmation-rate.sql
@@ -1,33 +1,7 @@
-# Write your MySQL query statement below
-/*
-SELECT S.user_id, COUNT(*) AS Total_requests
-FROM Signups AS S
-    LEFT JOIN Confirmations AS C
-    ON S.user_id = C.user_id
-GROUP BY S.user_id
-
-SELECT C.user_id, COUNT(*) AS Confirmed_requests
-FROM Signups AS S
-    LEFT JOIN Confirmations AS C
-    ON S.user_id = C.user_id
-GROUP BY C.user_id, C.action 
-HAVING C.action = 'confirmed';
-*/
-
-SELECT Tr.user_id, round(COALESCE(Confirmed_requests,0)/Total_requests,2) AS confirmation_rate
-FROM(
-    SELECT S.user_id, COUNT(*) AS Total_requests
-    FROM Signups AS S
-    LEFT JOIN Confirmations AS C
-    ON S.user_id = C.user_id
-    GROUP BY S.user_id
-) AS Tr
-LEFT JOIN (
-    SELECT C.user_id, COUNT(*) AS Confirmed_requests
-    FROM Signups AS S
-    LEFT JOIN Confirmations AS C
-    ON S.user_id = C.user_id
-    GROUP BY C.user_id, C.action 
-    HAVING C.action = 'confirmed'
-) AS Cr 
-ON Tr.user_id = Cr.user_id
+SELECT 
+    s.user_id,
+    ROUND(AVG(IF(c.action = 'confirmed', 1, 0)), 2) AS confirmation_rate
+FROM Signups AS s
+LEFT JOIN Confirmations AS c 
+    ON s.user_id = c.user_id
+GROUP BY s.user_id;
