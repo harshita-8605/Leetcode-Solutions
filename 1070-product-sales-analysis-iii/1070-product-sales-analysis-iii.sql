d8d45ddf9e99e166356ae00176f98f963da41440
harshita-8605 <harshitanarula08@gmail.com>
2026-09-11 15:18:52 +0530

    Time: 847 ms (30.23%), Space: 0B (100%) - LeetHub

--- 1070-product-sales-analysis-iii/1070-product-sales-analysis-iii.sql
+++ 1070-product-sales-analysis-iii/1070-product-sales-analysis-iii.sql
@@ -1,8 +1,11 @@
-# Write your MySQL query statement below
 SELECT product_id, year AS first_year, quantity, price
-FROM Sales
-WHERE (product_id, year) IN(
-    SELECT product_id, MIN(year)
+FROM (
+    SELECT 
+        product_id,
+        year,
+        quantity,
+        price,
+        MIN(year) OVER (PARTITION BY product_id) AS first_year
     FROM Sales
-    GROUP BY product_id
-)
+) AS S
+WHERE year = first_year;
