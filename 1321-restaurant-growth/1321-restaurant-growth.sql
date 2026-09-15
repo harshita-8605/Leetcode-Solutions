33a414b90bcb5979fdec51e9ac4f36000b0b182a
harshita-8605 <harshitanarula08@gmail.com>
2026-09-15 12:22:06 +0530

    Time: 462 ms (19.28%), Space: 0B (100%) - LeetHub

--- 1321-restaurant-growth/1321-restaurant-growth.sql
+++ 1321-restaurant-growth/1321-restaurant-growth.sql
@@ -1,31 +1,22 @@
-WITH daily AS (
-    SELECT 
-        visited_on,
-        SUM(amount) AS amount
+# Write your MySQL query statement below
+# https://www.youtube.com/watch?v=aM9GEJCCoUw
+SELECT C.visited_on,
+    (
+        SELECT SUM(amount)
+        FROM Customer 
+        WHERE visited_on BETWEEN DATE_SUB(C.visited_on, INTERVAL 6 DAY) AND C.visited_on
+    ) AS amount,
+    (
+        SELECT ROUND(SUM(amount)/7,2)
+        FROM Customer 
+        WHERE visited_on BETWEEN DATE_SUB(C.visited_on, INTERVAL 6 DAY) AND C.visited_on
+    ) AS average_amount
+
+
+FROM Customer AS C
+WHERE visited_on >= (
+    SELECT DATE_ADD(MIN(visited_on), INTERVAL 6 DAY)
     FROM Customer
-    GROUP BY visited_on
-),
-moving AS (
-    SELECT
-        visited_on,
-        amount,
-        SUM(amount) OVER (
-            ORDER BY visited_on
-            ROWS BETWEEN 6 PRECEDING AND CURRENT ROW
-        ) AS total_amount,
-        AVG(amount) OVER (
-            ORDER BY visited_on
-            ROWS BETWEEN 6 PRECEDING AND CURRENT ROW
-        ) AS average_amount,
-        ROW_NUMBER() OVER (
-            ORDER BY visited_on
-        ) AS rn
-    FROM daily
 )
-SELECT
-    visited_on,
-    total_amount AS amount,
-    ROUND(average_amount, 2) AS average_amount
-FROM moving
-WHERE rn >= 7
-ORDER BY visited_on;
+GROUP BY visited_on
+ORDER BY visited_on ASC
